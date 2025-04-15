aec552bf874ad5752cd7eb90c8664e68979a853a
5quidwyrm <henochtang09@gmail.com>
2025-04-15 13:05:20 +0100

    Used as statements to make code more compact.

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,26 @@ fn main() {
 
 #[test]
 fn loops() {
+    use forest_runtime::ForestInstruction as fi;
+    use forest_runtime::ForestValue as fv;
     let mut runtime = ForestRuntime::new(&[
-        ForestInstruction::Push(ForestValue::Int(10)),
-        ForestInstruction::Loop,
-        ForestInstruction::Duplicate,
-        ForestInstruction::Print,
-        ForestInstruction::Push(ForestValue::String("\n".to_string())),
-        ForestInstruction::Print,
-        ForestInstruction::Push(ForestValue::Int(1)),
-        ForestInstruction::Subtract,
-        ForestInstruction::Duplicate,
-        ForestInstruction::Push(ForestValue::Int(0)),
-        ForestInstruction::Eq,
-        ForestInstruction::If,
-        ForestInstruction::Exit,
-        ForestInstruction::IfEnd,
-        ForestInstruction::Drop,
-        ForestInstruction::LoopEnd,
-        ForestInstruction::Exit,
+        fi::Push(fv::Int(10)),
+        fi::Loop,
+        fi::Duplicate,
+        fi::Print,
+        fi::Push(fv::String("\n".to_string())),
+        fi::Print,
+        fi::Push(fv::Int(1)),
+        fi::Subtract,
+        fi::Duplicate,
+        fi::Push(fv::Int(0)),
+        fi::Eq,
+        fi::If,
+        fi::Exit,
+        fi::IfEnd,
+        fi::Drop,
+        fi::LoopEnd,
+        fi::Exit,
     ]);
     execute_runtime!(runtime, false);
 }
