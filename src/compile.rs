4cee8aaf21e3fc6a3660d7125a9340ecc2ade51c
5quidwyrm <henochtang09@gmail.com>
2025-04-17 22:15:42 +0100

    Finally implemented running raw files from text.

---
+++ src/compile.rs
@@ -0,0 +1,103 @@
+use crate::forest_runtime::{ForestInstruction as fi, ForestValue};
+use std::fmt;
+
+pub enum ForestCompileError {
+    OutOfBoundsWord,
+    // this means a word that doesn't end by the time the program ends compilation.
+}
+
+impl fmt::Display for ForestCompileError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::OutOfBoundsWord => write!(f, "Makeword (:) was last instruction!"),
+        }
+    }
+}
+
+pub fn parse_number(inpt: &str) -> Option<fi> {
+    match inpt.parse::<i64>() {
+        Ok(i) => Some(fi::Push(ForestValue::Int(i))),
+        Err(_) => None,
+    }
+}
+
+pub fn parse_string(inpt: &str) -> Option<fi> {
+    if inpt.len() > 1
+        && inpt.chars().nth(0).unwrap() == '\"'
+        && inpt.chars().nth_back(0).unwrap() == '\"'
+    {
+        Some(fi::Push(ForestValue::String(
+            inpt[1..(inpt.len() - 1)].to_string(),
+        )))
+    } else {
+        None
+    }
+}
+
+pub fn parse_nil(inpt: &str) -> Option<fi> {
+    if inpt == "nil" {
+        Some(fi::Push(ForestValue::Nil))
+    } else {
+        None
+    }
+}
+
+pub fn parse_table(inpt: &str) -> Option<fi> {
+    if inpt == "{}" {
+        Some(fi::Push(ForestValue::Table(vec![])))
+    } else {
+        None
+    }
+}
+
+pub fn compile(programstr: &str) -> Result<Vec<fi>, ForestCompileError> {
+    let mut tokens = programstr.split_whitespace();
+    let mut program: Vec<fi> = Vec::new();
+    'compilation: loop {
+        if let Some(tk) = tokens.next() {
+            match tk {
+                "dup" => program.push(fi::Duplicate),
+                "drop" => program.push(fi::Drop),
+                "+" => program.push(fi::Add),
+                "-" => program.push(fi::Subtract),
+                "*" => program.push(fi::Multiply),
+                "/" => program.push(fi::Divide),
+                "str" => program.push(fi::Stringify),
+                "<>" => program.push(fi::Concatenate),
+                "." => program.push(fi::Print),
+                "get" => program.push(fi::Get),
+                "assoc" => program.push(fi::Associate),
+                "keys" => program.push(fi::Keys),
+                "if" => program.push(fi::If),
+                "ifend" => program.push(fi::IfEnd),
+                "&" => program.push(fi::And),
+                "|" => program.push(fi::Or),
+                "!" => program.push(fi::Not),
+                "=" => program.push(fi::Eq),
+                "[" => program.push(fi::Loop),
+                "]" => program.push(fi::LoopEnd),
+                "break" => program.push(fi::Break),
+                ":" => {
+                    if let Some(tk) = tokens.next() {
+                        program.push(fi::MakeWord(tk.to_string()))
+                    } else {
+                        return Err(ForestCompileError::OutOfBoundsWord);
+                    }
+                }
+                ";" => program.push(fi::EndWord),
+                "exit" => program.push(fi::Exit),
+                s => program.push({
+                    parse_number(s)
+                        .or_else(|| parse_string(s))
+                        .or_else(|| parse_nil(s))
+                        .or_else(|| parse_table(s))
+                        .or_else(|| Some(fi::InvokeWord(s.to_string())))
+                        .unwrap()
+                }),
+            }
+        } else {
+            break 'compilation;
+        }
+    }
+    Ok(program)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,75 @@
 mod forest_runtime;
 use forest_runtime::*;
+mod compile;
+use compile::compile;
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
-fn main() {
+fn main() -> i32 {
     use forest_runtime::ForestInstruction::*;
-    let mut runtime = forest_runtime::ForestRuntime::new(&[Exit]);
-    execute_runtime!(runtime, false);
+    let mut args = env::args();
+    args.next().expect("Something went horribly wrong - there should be at least one argument being the name of the program");
+    match args.next() {
+        Some(a) => match a.as_str() {
+            "run" => {}
+            s => {
+                eprintln!("Unknown option {s} - try running `forest`");
+                return 1;
+            }
+        },
+        None => {
+            println!(
+                "
+                Welcome to the forest compiler!\n\
+                info: Usage: forest [command] {{options}}\n\n\
+                Commands:\n\n\
+                  run  | run from source\n\
+                Options:\n\
+                  dump | dump stack during runtime\n\
+                "
+            );
+            return 0;
+        }
+    }
+    if let Some(filepath) = args.next() {
+        if let Ok(mut file) = File::open(&filepath) {
+            let mut program = String::new();
+            match file.read_to_string(&mut program) {
+                Ok(_) => {}
+                Err(e) => {
+                    eprintln!("Could not read file, reason: {e}");
+                    return 1;
+                }
+            };
+            let instrs = compile(&program)
+                .map_err(|err| {
+                    eprintln!("Error in compilation: {err}");
+                    return 1;
+                })
+                .unwrap();
+            let mut runtime = ForestRuntime::new(&instrs);
+            let dump = match args.next() {
+                Some(o) => {
+                    if o == "dump" {
+                        true
+                    } else {
+                        eprintln!("Unknown option {o}");
+                        return 1;
+                    }
+                }
+                _ => false,
+            };
+            execute_runtime!(runtime, dump);
+        } else {
+            eprintln!("Cannot find {filepath} in current directory");
+            return 1;
+        }
+    } else {
+        eprintln!("Please provide a file name!");
+        return 1;
+    }
+    return 0;
 }
 
 #[test]
